81fd86b925c4bfd952dfb8d57677e753d73333f1
MohammadShohelhan <shohel11999khan@gmail.com>
2024-08-09 12:02:09 +0600

    my second assingment

---
+++ script.js
@@ -0,0 +1,75 @@
+//   const cursor = document.querySelector(".cursor");
+//   const body = document.querySelector("header");
+ 
+//   // console.log(cursor);
+
+//   body.addEventListener("mousemove",function(val){
+
+// console.log(val.x)
+//   gsap.to(cursor,{
+//     x:val.x - 220,
+//     y:val.y,
+//     duration:.4
+  
+//   })
+  
+// });
+
+const loader = document.querySelector(".preloader");
+window.addEventListener("load", function(){
+loader.style.display = 'none'
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const arr = ["images/image4.png", "images/image4.png","images/trainer4.png", "images/trainer3.png","images/trainer6.png"] ;
+     
+// let slider = document.querySelectorAll(".slider");
+// let love = document.querySelector(".love");
+// let hate= document.querySelectorAll(".hate");
+// console.log(love)
+
+// let score = 0;
+// // love.addEventListener("click", function (){
+// //   console.log("love");
+
+// // });
+
+// // console.log(slider);
+
+
+//      function next2() {
+//          score++;
+//          if(score >= arr.length){
+//              score = 0;
+
+//                }
+//                slider.src= arr[score];
+
+//           }
+
+
+
+
+
+// function previews() {
+//                     score--;
+//                   if(score < 0){
+//                        score = arr.length -1;
+   
+//                       }
+//              img.src= arr[score]
+//          }
+
+
